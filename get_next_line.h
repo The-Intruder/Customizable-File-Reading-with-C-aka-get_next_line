b7fd67d0efc64be1310d427b25e9deaad887a946
Mohamed amine Naimi <mnaimi@e2r12p2.1337.ma>
2021-11-22 21:45:58 +0100

    Backing up files, code not yet started

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnaimi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/21 15:59:44 by mnaimi            #+#    #+#             */
+/*   Updated: 2021/11/22 21:44:30 by mnaimi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef
+# define GET_NEXT_LINE_H
+
+/*  Testing Phase Includes  */
+/* ------- WARNING -------- */
+/* These should be removed  */
+/*    before final build    */
+# include<stdio.h>
+/* ------------------------ */
+
+/* Mandatory includes */
+# include <fcntl.h>
+# include <unistd.h>
+
+/* Macros */
+
+# define 
+
+#endif
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int		fd;
+	char	*buf;
+	int		read_bytes;
+
+	fd = open("file.txt", O_RDONLY);
+	if (fd < )
+	return 0;
+}
